2218179f52e0fff18d9dd6640ca82ffec649521f
Martin Zurowietz <martin@zurowietz.de>
2018-09-17 13:57:39 +0200

    Allow config vial env variables
    Resolves #4

--- src/config/demo.php
+++ src/config/demo.php
@@ -5,17 +5,17 @@ return [
     /*
     | ID of the (public) label tree to attach to each demo project.
     */
-    'label_tree_id' => null,
+    'label_tree_id' => env('DEMO_LABEL_TREE_ID', null),
 
     /*
     | ID of the volume to attach to each demo project. The volume is cloned without
     | existing annotations, i.e. each demo project gets its own volume.
     */
-    'volume_id' => null,
+    'volume_id' => env('DEMO_VOLUME_ID', null),
 
     /*
     | Name of each new demo project.
     */
-    'project_name' => 'Demo Project',
+    'project_name' => env('DEMO_PROJECT_NAME', 'Demo Project'),
 
 ];
