050b23e31bbe34903fc3f675c16f2cc41b82a0f3
Martin Zurowietz <martin@zurowietz.de>
2022-03-09 10:58:20 +0100

    Use api middleware for API routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 $router->group(
-    ['middleware' => 'auth:web,api', 'namespace' => 'Api', 'prefix' => 'api/v1'],
+    ['middleware' => ['api', 'auth:web,api'], 'namespace' => 'Api', 'prefix' => 'api/v1'],
     function ($router) {
         $router->post('projects/demo', 'DemoProjectController@store');
     }
